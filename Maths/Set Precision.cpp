b3c112094024c9e7c208a679b26e74ba410b0180
Rishabh Yadav <1998rishabh31@gmail.com>
2021-10-17 21:30:33 +0530

    Create Set Precision.cpp

---
+++ Maths/Set Precision.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    float a,b;
+	    cin>>a>>b;
+	    
+	    cout<<fixed<<std::setprecision(6);
+	    cout<<a*b<<"\n";
+	}
+	return 0;
+}
