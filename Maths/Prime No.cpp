264b6aebf36f6413d25720bf7bc8413b7077a56c
Rishabh Yadav <1998rishabh31@gmail.com>
2021-12-27 20:12:44 +0530

    Update Prime No.cpp

--- Maths/Prime No.cpp
+++ Maths/Prime No.cpp
@@ -2,6 +2,30 @@
 #include<cmath>
 using namespace std;
 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool isPrime(int n)
+{
+    if(n == 0 || n == 1)
+    return false;
+    
+    if(n == 2 || n == 3)
+    return true;
+    
+    if(n%2 == 0 || n%3 == 0)
+    return false;
+    
+    for(int i=5; i*i<=n; i=i+6) //we increment in terms of 6 because most prime number are in pattern of 6n-1 or 6n+1
+    if(n%i == 0 || n%(i+2) == 0)
+    return false;
+    
+    return true;
+
+}
+
+
 bool isPrime(long long n)
 {
     
