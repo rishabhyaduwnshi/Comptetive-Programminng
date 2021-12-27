2a3a63dc9701b9747021dca4daabac721725831c
Rishabh Yadav <1998rishabh31@gmail.com>
2021-12-27 20:03:58 +0530

    Update LCM & HCF.cpp

--- Maths/LCM & HCF.cpp
+++ Maths/LCM & HCF.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 
 
+int findHCF(int a,int b)
+{
+    if(b == 0)
+        return a;
+    else
+        return findHCF(b,a%b);
+}
+
 
 int main()
 {
-    int t;
-    cin>>t;
     
-    while(t--)
-    {
         long long a,b,lcm;
         cin>>a>>b;
         lcm = (a*b)/__gcd(a,b);
         cout<<__gcd(a,b)<<" "<<lcm<<" \n";
-        
-        
-    }
+    
+        cout<<findHCF(a,b);
 }
