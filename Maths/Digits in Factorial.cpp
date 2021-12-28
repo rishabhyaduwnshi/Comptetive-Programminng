6f7d1e018935ae835b5028fc74cf8012e09c751e
Rishabh Yadav <1998rishabh31@gmail.com>
2021-12-28 19:36:45 +0530

    Create Digits in Factorial.cpp

---
+++ Maths/Digits in Factorial.cpp
@@ -0,0 +1,15 @@
+int digitsInFactorial(int N)
+    {
+        if(N<0)
+        return 0;
+        
+        if(N==0 || N==1)
+        return 1;
+        
+        double digits = 0;
+        
+        for(int i=2;i<=N;i++)
+        digits = digits+log10(i);
+        
+        return floor(digits)+1;
+    }
