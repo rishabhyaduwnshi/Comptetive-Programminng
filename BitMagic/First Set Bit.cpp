be8ada81fc76cc0ba27afc472678658720a43be1
Rishabh Yadav <1998rishabh31@gmail.com>
2022-01-02 17:25:44 +0530

    Update First Set Bit.cpp

--- BitMagic/First Set Bit.cpp
+++ BitMagic/First Set Bit.cpp
@@ -2,6 +2,16 @@
 // AND operation of n and -n gives 1 only at the position of first set bit and rest bits becomes zero.
 // Then find log2 of the result and add 1 to get the answer. 
 
+
+//ffs is an inbuilt function which returns the position of first set Bit
+void printBinary(int n)
+{
+    cout<<ffs(n)<<endl;
+}
+
+
+
+
 unsigned int getFirstSetBit(int n)
     {
         if(n == 0)
