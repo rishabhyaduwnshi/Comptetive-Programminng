2c7399a0aa3365d7a1ee1ead61907d249ea3ecfa
Rishabh Yadav <1998rishabh31@gmail.com>
2022-03-09 00:54:09 +0530

    Create Pairwise Consecutive Elements.cpp

---
+++ Stack/Pairwise Consecutive Elements.cpp
@@ -0,0 +1,28 @@
+//https://practice.geeksforgeeks.org/problems/pairwise-consecutive-elements/1/?page=1&difficulty[]=-1&status[]=unsolved&category[]=Stack&sortBy=submissions#
+
+bool pairWiseConsecutive(stack<int> s)
+{
+    //Code here
+	stack<int> aux;
+	int aux_size=0;
+	while (!s.empty()) {
+		aux.push(s.top());
+		s.pop();
+		aux_size++;
+	}
+	bool result = true;
+	while (aux_size>1) {
+		int x = aux.top();
+		aux.pop();
+		int y = aux.top();
+		aux.pop();
+		if (abs(x - y) != 1)result = false;
+		s.push(x);
+		s.push(y);
+		aux_size--;
+		aux_size--;
+	}
+	if (aux.size() == 1)
+		s.push(aux.top());
+	return result;
+}
