4f29555dfd3a2a2892d395df159b33e84094e1c8
Rishabh Yadav <1998rishabh31@gmail.com>
2022-03-29 22:40:28 +0530

    Update Euclidean Algorithm.cpp

--- Maths/Modular Arithmetic/Euclidean Algorithm.cpp
+++ Maths/Modular Arithmetic/Euclidean Algorithm.cpp
@@ -8,5 +8,5 @@ int findHCF(int a,int b)
 
 // Important Points
 // The value of b reduces in every next function call
-// If b is higheer in first call, a & b are swapped in next function call
+// If b is higher in first call, a & b are swapped in next function call
 // b is always smaller than a, except possibly the first call 
