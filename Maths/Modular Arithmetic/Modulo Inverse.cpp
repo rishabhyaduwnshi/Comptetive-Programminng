5cb262a20ecf89806d6d727078c972e82a353340
Rishabh Yadav <1998rishabh31@gmail.com>
2022-03-29 22:31:39 +0530

    Create Modulo Inverse.cpp

---
+++ Maths/Modular Arithmetic/Modulo Inverse.cpp
@@ -0,0 +1,12 @@
+void moduloInverse(int n, int mod)
+{
+  for(int i=1;i<m;i++)
+  {
+    if((a*i) % m) == 1)
+    {
+      cout<<i<<endl;
+      return;
+    }
+  }
+ cout<<"Modulo Inverse Does Not Exists";
+}
